883e40142e0a27351d9dc80990cc9b385a83dbaa
Tumiso <Tumiso@MyLaptop>
2024-08-30 11:20:50 +0200

    Commit

---
+++ src/main/java/com/mycompany/iceactiviity1/Iceactiviity1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iceactiviity1;
+
+/**
+ *
+ * @author Tumiso
+ */
+public class Iceactiviity1 {
+
+    public static void main(String[] args) {
+      //declaration
+       String name, surname, email, number;
+       
+               
+       name = "Wandile";
+       surname = "Manzini";
+       email = "wandilemanzini@gmail.com";
+       number = "073156329";
+       
+       System.out.println("Full name:" + name + " " + surname);
+       System.out.println("email:" + email);
+       System.out.println("number:" + number);
+       
+       
+       
+    }
+}
